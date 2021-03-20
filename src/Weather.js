9df88a7fb63f982a5d46e3fef8830ee2001440a2
Cathrina <cathrina.hao@unity3d.com>
2021-03-20 22:04:11 +0900

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
@@ -77,8 +77,8 @@ export default function Weather() {
     );
   } else {
     const apiKey = "2076e06bc0e052a27f417aa4bccf1283";
-    let city = "New York";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading...";
